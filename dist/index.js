11c3e91c240d115ef02fa185ce7995057eece639
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-06 15:41:22 +0500

    Enums

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,21 @@
 "use strict";
 let age = 30;
+let sales = 123456789;
+let course = 'TypeString';
+let is_published = true;
+let level = 5;
+// Arrays
+let arr = [1, 2, 3, 4, 5];
+let arr2 = [1, 2, 3];
+// Tuples
+let user = [1, 'Akmal'];
+// Enums
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+;
+let mySize = Size.Medium;
+console.log(mySize);
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,21 @@ let age: number = 30
 
 let sales: number = 123_456_789;
 let course: string = 'TypeString';
-is_published: boolean = true;
+let is_published:boolean = true;
 
 let level = 5;
 
 // Arrays
-let arr: number[] = [1,2,3,4,5]
-let arr2 = [1,2,3]
+let arr: number[] = [1,2,3,4,5];
+let arr2 = [1,2,3];
 
 // Tuples
-let user: [number, string] = [1, 'Akmal']
+let user: [number, string] = [1, 'Akmal'];
+
+// Enums
+enum Size {Small=1,Medium,Large};
+let mySize: Size = Size.Medium;
+console.log(mySize)
 
 
 
