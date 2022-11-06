47e8fbc4ebe5bfc4e65e6bec2aadc0180fe1278a
Akmal Rizayev <akmal.rizayev@gmail.com>
2022-11-06 15:12:33 +0500

    Built-in Types

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ npm i g typescript
 tsc index.ts
 
 tsc --init
+
+# Built-in Types
+
+any
+unknown
+never
+enum
+tuple
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 let age: number = 30
 
+let sales: number = 123_456_789;
+let course: string = 'TypeString';
+is_published: boolean = true;
+
+let level = 5;
+
 
