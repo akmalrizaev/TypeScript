52c45ff2ea52c56db9a21d8914bdad9683da3553
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-09-29 14:38:09 +0500

    typescript variable types

---
+++ script.js
@@ -0,0 +1,15 @@
+var firstName = 'Akmal';
+var x = 5;
+var isMarried = false;
+var colors = ['red', 'green', 'blue'];
+var numbers = [1, 2, 3, 4, 5];
+var smth = 1;
+console.log(smth);
+smth = ['blac', 'white'];
+console.log(smth);
+smth = {
+    name: 'bmw',
+    color: 'black'
+};
+console.log(smth);
+console.log(name);
---
+++ script.ts
@@ -0,0 +1,23 @@
+const firstName: string = 'Akmal';
+
+const x: number = 5;
+
+const isMarried: boolean = false;
+
+const colors: string[] = ['red', 'green', 'blue'];
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+let smth: any = 1;
+console.log(smth);
+
+smth = ['blac', 'white'];
+console.log(smth);
+
+smth = {
+  name: 'bmw',
+  color: 'black',
+};
+console.log(smth);
+
+console.log(name);
