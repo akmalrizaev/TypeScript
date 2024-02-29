2dc9e210f4b39402cf180056a199313fc92e782d
akmalrizaev <akmaljon.rizayev@gmail.com>
2024-02-29 16:17:03 +0500

    Writing our first Typescript Class The Constructor

--- src/classes/classes-intro.js
+++ src/classes/classes-intro.js
@@ -1,2 +1,16 @@
-var date = new Date();
-console.log(date.getTime());
+// const date = new Date();
+// console.log(date.getTime());
+var Course = /** @class */ (function () {
+    function Course(title, subtitle, creationDt) {
+        this.title = title;
+        this.subtitle = subtitle;
+        this.creationDt = creationDt;
+    }
+    Course.prototype.age = function () {
+        var ageInMs = new Date().getTime() - this.creationDt.getTime();
+        return Math.round(ageInMs / 1000 / 60 / 24); // age in days
+    };
+    return Course;
+}());
+var mycourse = new Course('Typesript Bootcamp', 'Learn the language fundamentals', new Date(2020, 1, 1));
+console.log(mycourse.age());
--- src/classes/classes-intro.ts
+++ src/classes/classes-intro.ts
@@ -1,3 +1,28 @@
-const date = new Date();
+// const date = new Date();
 
-console.log(date.getTime());
+// console.log(date.getTime());
+
+class Course {
+  title: string;
+  subtitle: string;
+  creationDt: Date;
+
+  constructor(title: string, subtitle: string, creationDt: Date) {
+    this.title = title;
+    this.subtitle = subtitle;
+    this.creationDt = creationDt;
+  }
+
+  age() {
+    const ageInMs = new Date().getTime() - this.creationDt.getTime();
+    return Math.round(ageInMs / 1000 / 60 / 24); // age in days
+  }
+}
+
+const mycourse = new Course(
+  'Typesript Bootcamp',
+  'Learn the language fundamentals',
+  new Date(2020, 1, 1)
+);
+
+console.log(mycourse.age());
